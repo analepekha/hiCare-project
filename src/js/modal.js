8755dac0fe4a95af030f1cd1e9726161ffa0039d
analepekha <maiboroda0103@gmail.com>
2023-04-15 14:27:23 +0300

    add script modal and contract page

--- src/js/modal.js
+++ src/js/modal.js
@@ -28,7 +28,10 @@ document.addEventListener('DOMContentLoaded', function () {
       Подложке зададим id, чтобы не влиять на другие элементы с классом overlay*/
   var modalButtons = document.querySelectorAll('.js-open-modal'),
     overlay = document.querySelector('.js-overlay-modal'),
-    closeButtons = document.querySelectorAll('.js-modal-close');
+    closeButtons = document.querySelectorAll('.js-modal-close'),
+    modal = document.querySelector('js-modal');
+
+  const body = document.querySelector('body');
 
   /* Перебираем массив кнопок */
   modalButtons.forEach(function (item) {
@@ -49,6 +52,7 @@ document.addEventListener('DOMContentLoaded', function () {
             подложке и окну чтобы показать их. */
       modalElem.classList.add('active');
       overlay.classList.add('active');
+      body.classList.add('unscroll');
     }); // end click
   }); // end foreach
 
@@ -58,6 +62,7 @@ document.addEventListener('DOMContentLoaded', function () {
       console.log(parentModal);
       parentModal.classList.remove('active');
       overlay.classList.remove('active');
+      body.classList.remove('unscroll');
     });
   }); // end foreach
 
@@ -69,6 +74,7 @@ document.addEventListener('DOMContentLoaded', function () {
       if (key == 27) {
         document.querySelector('.modal.active').classList.remove('active');
         document.querySelector('.overlay').classList.remove('active');
+        body.classList.remove('unscroll');
       }
     },
     false,
@@ -77,55 +83,6 @@ document.addEventListener('DOMContentLoaded', function () {
   overlay.addEventListener('click', function () {
     document.querySelector('.modal.active').classList.remove('active');
     overlay.classList.remove('active');
+    body.classList.remove('unscroll');
   });
 }); // end ready
-
-// !(function (e) {
-//   'function' != typeof e.matches &&
-//     (e.matches =
-//       e.msMatchesSelector ||
-//       e.mozMatchesSelector ||
-//       e.webkitMatchesSelector ||
-//       function (e) {
-//         for (
-//           var t = this, o = (t.document || t.ownerDocument).querySelectorAll(e), n = 0;
-//           o[n] && o[n] !== t;
-
-//         )
-//           ++n;
-//         return Boolean(o[n]);
-//       }),
-//     'function' != typeof e.closest &&
-//       (e.closest = function (e) {
-//         for (var t = this; t && 1 === t.nodeType; ) {
-//           if (t.matches(e)) return t;
-//           t = t.parentNode;
-//         }
-//         return null;
-//       });
-// })(window.Element.prototype);
-
-// document.addEventListener('DOMContentLoaded', function () {
-//   const modalButtons = document.querySelectorAll('.js-open-modal');
-//   const overlay = document.querySelector('#overlay-modal');
-//   const closeButtons = document.querySelector('.js-modal-close');
-
-//   modalButtons.forEach(function (item) {
-//     item.addEventListener('click', function (e) {
-//       e.preventDefault();
-//       var modalId = this.getAttribute('data-modal'),
-//         modalElem = document.querySelector('.modal[data-modal="' + modalId + '"]');
-
-//       modalElem.classList.add('active');
-//       overlay.classList.add('active');
-//     }); // end click
-//   }); // end foreach
-
-//   closeButtons.forEach(function (item) {
-//     item.addEventListener('click', function (e) {
-//       console.log(this.closest('.modal'));
-//     });
-//   });
-// }); // end ready
-
-// // end foreach
