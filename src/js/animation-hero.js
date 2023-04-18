f08705d0b9f7b3ef256adfb73102488f77451205
analepekha <maiboroda0103@gmail.com>
2023-04-18 23:38:42 +0300

    update data

--- src/js/animation-hero.js
+++ src/js/animation-hero.js
@@ -60,7 +60,7 @@ const fx = new TextScramble(el);
 let counter = 0;
 const next = () => {
   fx.setText(phrases[counter]).then(() => {
-    setTimeout(next, 3000);
+    setTimeout(next, 1000);
   });
   counter = (counter + 1) % phrases.length;
 };
--- src/js/header-modal.js
+++ src/js/header-modal.js
@@ -1,13 +1,43 @@
-(() => {
-  const menuBtnRef = document.querySelector('[data-menu-button]');
-  const mobileMenuRef = document.querySelector('[data-header-menu]');
-  const mobileBtnClose = document.querySelector('[data-menu-close]');
+// (() => {
+//   const menuBtnRef = document.querySelector('[data-menu-button]');
+//   const mobileMenuRef = document.querySelector('[data-header-menu]');
+//   const mobileBtnClose = document.querySelector('[data-menu-close]');
 
-  menuBtnRef.addEventListener('click', () => {
-    mobileMenuRef.classList.toggle('is-open');
-  });
+//   menuBtnRef.addEventListener('click', () => {
+//     mobileMenuRef.classList.toggle('is-open');
+//   });
+//   mobileBtnClose.addEventListener('click', () => {
+//     mobileMenuRef.classList.toggle('is-open');
+//   });
+// })();
 
-  mobileBtnClose.addEventListener('click', () => {
-    mobileMenuRef.classList.toggle('is-open');
-  });
-})();
+const menuBtn = document.querySelector('#hamb');
+const mobileMenuRef = document.querySelector('[data-header-menu]');
+const menuBtnClose = document.querySelector('[data-menu-close]');
+const body = document.body;
+
+menuBtn.addEventListener('click', menuOpenHandler);
+// menuBtnClose.addEventListener('click', menuCloseHandler);
+
+function menuOpenHandler(e) {
+  e.preventDefault();
+  mobileMenuRef.classList.toggle('is-open');
+  menuBtn.classList.toggle('active');
+  body.classList.toggle('noscroll');
+}
+// function menuCloseHandler(e) {
+//   e.preventDefault();
+//   mobileMenuRef.classList.toggle('is-open');
+// }
+
+const links = Array.from(mobileMenuRef.children);
+
+links.forEach(link => {
+  link.addEventListener('click', closeOnClick);
+});
+
+function closeOnClick() {
+  mobileMenuRef.classList.remove('is-open');
+  menuBtn.classList.remove('active');
+  body.classList.remove('noscroll');
+}
